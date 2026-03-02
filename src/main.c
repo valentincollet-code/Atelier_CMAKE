8d75f8a765fe58e620bb817335e08b81f0b13494
valentincollet-code <v.colletpro@gmail.com>
2026-03-02 23:27:50 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 #include "./../include/calculator.h"
 
 int main(int argc, char *argv[]){
-	if (argc == 4)
+	if (argc == 3 || argc == 4)
 	{
 	char* op = argv[1];
 	char* a = argv[2];
@@ -26,6 +26,10 @@ int main(int argc, char *argv[]){
 		r = _div(atof(a), atof(b));
 		printf("%lf",r);
 	}
+		else if (strcmp(op, "car") == 0) {
+    r = _square(atof(a)); // On suppose que votre fonction s'appelle _square
+    printf("%lf", r);
+	}		
 	else {printf("Erreur de parametres");}
 	}
 	else {printf("Erreur de parametres");}
