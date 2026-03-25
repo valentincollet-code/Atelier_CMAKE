772911409e455c0da7d6e08d2beab998c72d3f70
valentincollet-code <valentin.collet@2028.icam.fr>
2026-03-25 21:56:27 +0100

    Exercice 1: Update main.c

--- src/main.c
+++ src/main.c
@@ -10,6 +10,9 @@ int main(int argc, char *argv[]){
 	char* a = argv[2];
 	char* b = argv[3];
 	double r = 0;
+
+// Pour les opérations +, -, *, /, besoin de 4 arguments:
+		
 	if (strcmp(op, "add") == 0) {
 		r = _add(atof(a), atof(b));
 		printf("%lf",r);
@@ -28,6 +31,19 @@ int main(int argc, char *argv[]){
 	}
 	else {printf("Erreur de parametres");}
 	}
+// Pour opération carré besoin de 3 arguments seulement
+
+		else if (argc == 3) {
+        if (strcmp(argv[1], "car") == 0) {
+            double r = _sqr(atof(argv[2]));
+            printf("%lf", r);
+        } else {
+            printf("Erreur de parametres");
+        }
+    }
+
+// Si ce n'est ni 3 ni 4 arguments
+			
 	else {printf("Erreur de parametres");}
 	return 0;
 }
